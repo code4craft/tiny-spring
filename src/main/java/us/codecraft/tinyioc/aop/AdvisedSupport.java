5fef1c59f39627c1cb3b306481d60f9310ab20eb
yihua.huang <code4crafter@gmail.com>
2014-01-13 18:30:31 +0800

    method interceptor by jdk dynamic proxy

--- src/main/java/us/codecraft/tinyioc/aop/AdvisedSupport.java
+++ src/main/java/us/codecraft/tinyioc/aop/AdvisedSupport.java
@@ -1,7 +1,29 @@
 package us.codecraft.tinyioc.aop;
 
+import org.aopalliance.intercept.MethodInterceptor;
+
 /**
  * @author yihua.huang@dianping.com
  */
 public class AdvisedSupport {
+
+	private TargetSource targetSource;
+
+    private MethodInterceptor methodInterceptor;
+
+    public TargetSource getTargetSource() {
+        return targetSource;
+    }
+
+    public void setTargetSource(TargetSource targetSource) {
+        this.targetSource = targetSource;
+    }
+
+    public MethodInterceptor getMethodInterceptor() {
+        return methodInterceptor;
+    }
+
+    public void setMethodInterceptor(MethodInterceptor methodInterceptor) {
+        this.methodInterceptor = methodInterceptor;
+    }
 }
--- src/test/java/us/codecraft/tinyioc/HelloWorldService.java
+++ src/test/java/us/codecraft/tinyioc/HelloWorldService.java
@@ -3,21 +3,7 @@ package us.codecraft.tinyioc;
 /**
  * @author yihua.huang@dianping.com
  */
-public class HelloWorldService {
+public interface HelloWorldService {
 
-    private String text;
-
-    private OutputService outputService;
-
-    public void helloWorld(){
-        outputService.output(text);
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public void setOutputService(OutputService outputService) {
-        this.outputService = outputService;
-    }
+    void helloWorld();
 }
