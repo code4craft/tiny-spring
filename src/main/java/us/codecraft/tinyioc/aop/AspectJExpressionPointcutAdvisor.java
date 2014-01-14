6a4e4cf1300327d834d9a26d0261cbf851f52776
yihua.huang <code4crafter@gmail.com>
2014-01-14 22:09:07 +0800

    aspectj advisor

--- src/main/java/us/codecraft/tinyioc/aop/AspectJExpressionPointcutAdvisor.java
+++ src/main/java/us/codecraft/tinyioc/aop/AspectJExpressionPointcutAdvisor.java
@@ -7,9 +7,13 @@ import org.aopalliance.aop.Advice;
  */
 public class AspectJExpressionPointcutAdvisor implements PointcutAdvisor {
 
-    private AspectJExpressionPointcut pointcut;
+    private AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
 
     private Advice advice;
+
+    public void setAdvice(Advice advice) {
+        this.advice = advice;
+    }
 
     public void setExpression(String expression) {
         this.pointcut.setExpression(expression);
