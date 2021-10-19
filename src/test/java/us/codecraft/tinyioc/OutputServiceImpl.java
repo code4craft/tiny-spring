d77edb95c4879314103e6282603b6ef6a7a95bbd
DragonDreamer <luochong@whu.edu.cn>
2021-10-19 22:38:06 +0800

    只通过方法名反射获取set方法

--- src/test/java/us/codecraft/tinyioc/OutputServiceImpl.java
+++ src/test/java/us/codecraft/tinyioc/OutputServiceImpl.java
@@ -4,10 +4,9 @@ package us.codecraft.tinyioc;
  * @author yihua.huang@dianping.com
  */
 public class OutputServiceImpl implements OutputService {
-
+    private HelloWorldService helloWorldService;
     @Override
     public void output(String text){
         System.out.println(text);
     }
-
 }
